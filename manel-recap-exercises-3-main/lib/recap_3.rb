29d5488311bc5edcc4e00d7041c3575821b80e5e
Manel-Os <oulds.manel@gmail.com>
2023-05-11 16:40:56 -0400

    Recap3 Proc problems

--- manel-recap-exercises-3-main/lib/recap_3.rb
+++ manel-recap-exercises-3-main/lib/recap_3.rb
@@ -147,3 +147,55 @@ p vowel_rotate('bootcamp')      # => "baotcomp"
 p vowel_rotate('awesome')       # => "ewasemo"
 
 p "-------------------------------------------------------------"
+
+#Proc Problems
+
+class String
+    def select(&blc)
+        new_s=""
+        return "" if !blc
+        self.each_char.with_index do |c,i|
+            new_s+=c if blc.call(c)
+        end
+        new_s
+    end
+
+
+    def map!(&blc)
+        self.each_char.with_index do |c,i|
+            self[i] = blc.call(c,i)
+        end
+        self
+    end
+    
+    
+
+end
+
+p "app academy".select { |ch| !"aeiou".include?(ch) }   # => "pp cdmy"
+p "HELLOworld".select { |ch| ch == ch.upcase }          # => "HELLO"
+p "HELLOworld".select          # => ""
+
+p "-------------------------------------------------------------"
+
+word_1 = "Lovelace"
+word_1.map! do |ch| 
+    if ch == 'e'
+        '3'
+    elsif ch == 'a'
+        '4'
+    else
+        ch
+    end
+end
+p word_1        # => "Lov3l4c3"
+
+word_2 = "Dijkstra"
+word_2.map! do |ch, i|
+    if i.even?
+        ch.upcase
+    else
+        ch.downcase
+    end
+end
+p word_2        # => "DiJkStRa"
